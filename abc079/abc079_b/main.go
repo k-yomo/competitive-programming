d4c33021ee97b75b0c6c737d49b36ec98171e369
k-yomo <kanji.yy@gmail.com>
2020-03-14 00:31:34 +0900

    Solve A,B,C in ABC079

--- abc079/abc079_b/main.go
+++ abc079/abc079_b/main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// Code for B - Lucas Number
-	fmt.Printf("Hello world")
+	var n int
+	fmt.Scan(&n)
+	luca := make([]int, n+1)
+	luca[0] = 2
+	luca[1] = 1
+	for i := 2; i <= n; i++ {
+		luca[i] = luca[i-1] + luca[i-2]
+	}
+	fmt.Println(luca[n])
 }
--- abc079/abc079_c/main.go
+++ abc079/abc079_c/main.go
@@ -1,8 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+type op bool
+
+func (o op) String() string {
+	if o {
+		return "+"
+	} else {
+		return "-"
+	}
+}
 
 func main() {
-	// Code for C - Train Ticket
-	fmt.Printf("Hello world")
+	var n string
+	fmt.Scan(&n)
+
+	a, _ := strconv.Atoi(string(n[0]))
+	b, _ := strconv.Atoi(string(n[1]))
+	c, _ := strconv.Atoi(string(n[2]))
+	d, _ := strconv.Atoi(string(n[3]))
+
+	options := [][]op{
+		{true, false, false},
+		{true, true, false},
+		{true, true, true},
+		{true, false, true},
+		{false, true, true},
+		{false, false, true},
+		{false, true, false},
+		{false, false, false},
+	}
+
+	for _, op := range options {
+		total := a
+		if op[0] {
+			total += b
+		} else {
+			total -= b
+		}
+		if op[1] {
+			total += c
+		} else {
+			total -= c
+		}
+		if op[2] {
+			total += d
+		} else {
+			total -= d
+		}
+		if total == 7 {
+			fmt.Println(fmt.Sprintf("%d%s%d%s%d%s%d=7", a, op[0], b, op[1], c, op[2], d))
+			return
+		}
+	}
 }
