ff8b8ca947d073d46b19e654ab6c7bd9d514b293
k-yomo <kanji.yy@gmail.com>
2020-03-28 20:10:02 +0900

    Solev B, C, D in ABC154

--- abc154/abc154_b/main.go
+++ abc154/abc154_b/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// Code for B - I miss you...
-	fmt.Printf("Hello world")
+	var s string
+	fmt.Scan(&s)
+	for i := 0; i < len(s); i++ {
+		fmt.Print("x")
+	}
+	fmt.Println("" )
 }
