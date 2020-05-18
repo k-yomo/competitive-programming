ca2df33878d40d5e37500b309f1325313cdfcf1f
k-yomo <kanji.yy@gmail.com>
2020-05-18 22:20:08 +0900

    Solve B in ABC106

--- abc106/abc106_b/main.go
+++ abc106/abc106_b/main.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// Code for B - 105
-	fmt.Printf("Hello world")
+	var n int
+	fmt.Scan(&n)
+	var eightDivisorCount int
+	for i := 1; i <= n; i += 2 {
+		var count int
+		for j := 1; j <= n; j++ {
+			if i%j == 0 {
+				count++
+			}
+		}
+		if count == 8 {
+			eightDivisorCount++
+		}
+	}
+	fmt.Println(eightDivisorCount)
 }
