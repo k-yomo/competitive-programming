8bd632413080a146ad3895c3705253e486600698
k-yomo <kanji.yy@gmail.com>
2020-06-15 23:37:40 +0900

    Solve A,B in abc165

--- abc165/abc165_b/main.go
+++ abc165/abc165_b/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	// Code for B - 1%
-	fmt.Printf("Hello world")
+	var x int
+	fmt.Scan(&x)
+
+	balance := 100
+	year := 0
+	for balance < x {
+		balance = int(math.Floor(float64(balance) * 1.01))
+		year++
+	}
+	fmt.Println(year)
 }
