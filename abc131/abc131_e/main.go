da3a1a7af9fbcb4eb26fd569c6f8f7702869ecc5
k-yomo <kanji.yy@gmail.com>
2020-06-20 18:03:38 +0900

    Solve D in abc131

---
+++ abc131/abc131_e/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Code for E - Friendships
+	fmt.Printf("Hello world")
+}
---
+++ abc131/abc131_f/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Code for F - Must Be Rectangular!
+	fmt.Printf("Hello world")
+}
