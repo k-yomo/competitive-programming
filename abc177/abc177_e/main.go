a60a74fb5eb184aad3e6f0bf5ab3656f4f6f422e
k-yomo <kanji.yy@gmail.com>
2020-08-29 22:57:44 +0900

    Solve A, B, C, D in abc177

---
+++ abc177/abc177_e/main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	io, flush := NewIO()
+	defer flush()
+
+	n := io.ScanInt()
+	a := io.ScanInts(n)
+	curGCD := a[0]
+
+	isParwiseCoprime := true
+	for i := 0; i < n-1; i++ {
+		if !isParwiseCoprime {
+			break
+		}
+		curGCD = Gcd(curGCD, a[i])
+		for j := i+1; j < n; j++ {
+			if Gcd(a[i],a[j]) != 1 {
+				isParwiseCoprime = false
+				break
+			}
+		}
+	}
+	curGCD = Gcd(curGCD, a[n-1])
+	if isParwiseCoprime {
+		fmt.Println("pairwise coprime")
+	} else if curGCD == 1 {
+		fmt.Println("setwise coprime")
+	} else {
+		fmt.Println("not coprime")
+	}
+}
+
+// Gcd returns greatest common divisor
+func Gcd(x, y int) int {
+	mod := x % y
+	if mod > 0 {
+		return Gcd(y, mod)
+	} else {
+		return y
+	}
+}
+
+type IO struct {
+	scanner *bufio.Scanner
+	writer  *bufio.Writer
+}
+
+func NewIO() (*IO, func()) {
+	io := &IO{
+		scanner: newScanner(),
+		writer:  newWriter(),
+	}
+	return io, func() { io.writer.Flush() }
+}
+
+func newScanner() *bufio.Scanner {
+	s := bufio.NewScanner(os.Stdin)
+	s.Buffer(make([]byte, 10000000), 10000000)
+	s.Split(bufio.ScanWords)
+	return s
+}
+
+func newWriter() *bufio.Writer {
+	return bufio.NewWriter(os.Stdout)
+}
+
+func (io *IO) ScanBytes() []byte {
+	if !io.scanner.Scan() {
+		panic("scan string failed")
+	}
+	return io.scanner.Bytes()
+}
+
+func (io *IO) ScanString() string {
+	if !io.scanner.Scan() {
+		panic("scan string failed")
+	}
+	return io.scanner.Text()
+}
+
+func (io *IO) ScanStrings(n int) []string {
+	strs := make([]string, n)
+	for i := 0; i < n; i++ {
+		strs[i] = io.ScanString()
+	}
+	return strs
+}
+
+func (io *IO) Scan2DStrings(y, x int) [][]string {
+	strings := make([][]string, y)
+	for i := 0; i < y; i++ {
+		strings[i] = io.ScanStrings(x)
+	}
+	return strings
+}
+
+func (io *IO) Scan2DGraph(y int) [][]string {
+	strs := make([][]string, y)
+	for i := 0; i < y; i++ {
+		strs[i] = strings.Split(io.ScanString(), "")
+	}
+	return strs
+}
+
+func (io *IO) ScanInt() int {
+	return int(io.ScanInt64())
+}
+
+func (io *IO) ScanInt2() (int, int) {
+	return int(io.ScanInt64()), int(io.ScanInt64())
+}
+
+func (io *IO) ScanInt3() (int, int, int) {
+	return int(io.ScanInt64()), int(io.ScanInt64()), int(io.ScanInt64())
+}
+
+func (io *IO) ScanInt4() (int, int, int, int) {
+	return int(io.ScanInt64()), int(io.ScanInt64()), int(io.ScanInt64()), int(io.ScanInt64())
+}
+
+func (io *IO) ScanInts(n int) []int {
+	ints := make([]int, n)
+	for i := 0; i < n; i++ {
+		ints[i] = io.ScanInt()
+	}
+	return ints
+}
+
+func (io *IO) Scan2DInts(y, x int) [][]int {
+	ints := make([][]int, y)
+	for i := 0; i < y; i++ {
+		ints[i] = io.ScanInts(x)
+	}
+	return ints
+}
+
+func (io *IO) ScanInt64() int64 {
+	i, err := strconv.ParseInt(io.ScanString(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *IO) ScanFloat64() float64 {
+	i, _ := strconv.ParseFloat(io.ScanString(), 64)
+	return i
+}
+
+func (io *IO) ScanFloat64s(n int) []float64 {
+	floats := make([]float64, n)
+	for i := 0; i < n; i++ {
+		floats[i] = io.ScanFloat64()
+	}
+	return floats
+}
+
+func (io *IO) Println(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
---
+++ abc177/abc177_f/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Code for F - I hate Shortest Path Problem
+	fmt.Printf("Hello world")
+}
