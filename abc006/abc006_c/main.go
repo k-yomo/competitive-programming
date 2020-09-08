6abffe1675836d382f9427c8ccd8b39c9de58a04
k-yomo <kanji.yy@gmail.com>
2020-09-09 01:47:56 +0900

    Solve C in abc006

--- abc006/abc006_c/main.go
+++ abc006/abc006_c/main.go
@@ -1,8 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main()  {
+func main() {
 	var n, m int
 	fmt.Scan(&n, &m)
+
+	// 2人の老人は大人1 + 赤ちゃん1に分けられるため
+	if m%2 != 0 {
+		for adultCount := 0; adultCount < n; adultCount++ {
+			babyCount := n - 1 - adultCount
+			if adultCount*2+babyCount*4+3 == m {
+				fmt.Println(adultCount, 1, babyCount)
+				return
+			}
+		}
+	} else {
+		for adultCount := 0; adultCount <= n; adultCount++ {
+			babyCount := n - adultCount
+			if adultCount*2+babyCount*4 == m {
+				fmt.Println(adultCount, 0, babyCount)
+				return
+			}
+		}
+	}
+	fmt.Println("-1 -1 -1")
+}
+
+func main2() {
+	var n, m int
+	fmt.Scan(&n, &m)
+
+	for elderlyCount := 0; elderlyCount <= n; elderlyCount++ {
+		babyCount := (m - 2*n) / 2
+		adultCount := (-m + 4*n) / 2
+		if babyCount < 0 || adultCount < 0 {
+			continue
+		}
+
+		if adultCount+elderlyCount+babyCount == n && adultCount*2+elderlyCount*3+babyCount*4 == m {
+			fmt.Println(adultCount, elderlyCount, babyCount)
+			return
+		}
+	}
+	fmt.Println("-1 -1 -1")
 }
