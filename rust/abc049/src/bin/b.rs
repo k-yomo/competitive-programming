95f6a4a12c08e0335980ad6e275ebd9328526c3f
k-yomo <kanji.yy@gmail.com>
2020-10-05 01:56:45 +0900

    Solve B in abc049

---
+++ rust/abc049/src/bin/b.rs
@@ -0,0 +1,19 @@
+#![allow(unused_imports)]
+use proconio::*;
+use std::cmp::*;
+use std::collections::*;
+use std::io::Write;
+use std::ops::Bound::*;
+ 
+fn main() { 
+    input! {
+        h: usize,
+        _w: usize,
+        c: [String; h]
+    }
+
+    for s in c {
+        println!("{}", s);
+        println!("{}", s);
+    }
+}
