73e585f0d4d6c2d096c09c9621729bd6c0a08760
k-yomo <kanji.yy@gmail.com>
2020-12-04 00:14:35 +0900

    Solve D in abc183

--- rust/abc183/src/bin/d.rs
+++ rust/abc183/src/bin/d.rs
@@ -1,14 +1,31 @@
 #![allow(unused_imports)]
-use proconio::*;
-use proconio::marker::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
 
-fn main() { 
+use itertools::*;
+use proconio::*;
+use proconio::marker::*;
+
+fn main() {
     input! {
-        
+        n: usize,
+        w: i64,
+        stp: [(usize, usize, i64); n],
     }
+
+    let limit = 2_000_001;
+    let mut a: Vec<i64> = vec![0; limit];
+    for (s, t, p) in stp {
+        a[s] += p;
+        a[t] -= p;
+    }
+
+    for i in 0..limit - 1 {
+        a[i + 1] += a[i];
+    }
+
+    println!("{}", if a.iter().all(|&x| x <= w) { "Yes" } else { "No" });
 }
