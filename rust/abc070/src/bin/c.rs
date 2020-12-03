063c4831c5046de5467021a695abfcae2c88ae4d
k-yomo <kanji.yy@gmail.com>
2020-12-03 22:47:30 +0900

    Solve C in abc070

--- rust/abc070/src/bin/c.rs
+++ rust/abc070/src/bin/c.rs
@@ -5,9 +5,18 @@ use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
+use num_integer::lcm;
+
 fn main() { 
     input! {
-        
+        n: usize,
+        t: [usize; n],
     }
+
+    let mut ans = t[0];
+    for &n in t[1..].iter() {
+        ans = lcm(ans, n);
+    }
+
+    println!("{}", ans);
 }
