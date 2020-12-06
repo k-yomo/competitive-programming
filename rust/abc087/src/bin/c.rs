5b0cdb7c72ffab4fad1eaa28b342bba026a0e51a
k-yomo <kanji.yy@gmail.com>
2020-12-06 11:19:26 +0900

    Solve C in abc087

--- rust/abc087/src/bin/c.rs
+++ rust/abc087/src/bin/c.rs
@@ -5,9 +5,18 @@ use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
+use itertools_num::ItertoolsNum;
+use itertools::__std_iter::once;
+
 fn main() { 
     input! {
-        
+        n: usize,
+        a1: [usize; n],
+        a2: [usize; n],
     }
+
+    let a1_acc = a1.iter().cumsum().collect::<Vec<usize>>();
+    let a2_acc = once(0).chain(a2).cumsum().collect::<Vec<usize>>();
+
+    println!("{}", (0..n).map(|i| { a1_acc[i] + (a2_acc[n] - a2_acc[i]) }).max().unwrap());
 }
