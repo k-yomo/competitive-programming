b38f2ad6f8b4da38f0a4f42166bb9ceb3615d261
k-yomo <kanji.yy@gmail.com>
2020-12-06 11:42:45 +0900

    Solve C in abc083

--- rust/abc083/src/bin/c.rs
+++ rust/abc083/src/bin/c.rs
@@ -8,6 +8,17 @@ use std::ops::Bound::*;
  
 fn main() { 
     input! {
-        
+       mut x: usize,
+       y: usize,
     }
+
+    let mut count = 1;
+    while x <= y {
+        if x * 2 <= y {
+            count += 1;
+        }
+        x *= 2;
+    }
+
+    println!("{}", count);
 }
