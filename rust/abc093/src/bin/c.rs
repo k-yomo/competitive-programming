ddae18b524a44ae6d3524ab97293ee7ebaca874d
k-yomo <kanji.yy@gmail.com>
2020-12-06 09:11:37 +0900

    Solve C in abc093

--- rust/abc093/src/bin/c.rs
+++ rust/abc093/src/bin/c.rs
@@ -1,12 +1,26 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use proconio::*;
+
+fn main() {
     input! {
-        
+        mut nums: [usize; 3],
     }
+
+    nums.sort();
+    let (mut min, mut mid, mut max) = (nums[0], nums[1], nums[2]);
+    let mut count = 0;
+    count += max - mid;
+    min += count;
+    count += (max - min + 1) / 2;
+    if (max - min) % 2 != 0 {
+        count += 1;
+    }
+
+    println!("{}", count);
 }
