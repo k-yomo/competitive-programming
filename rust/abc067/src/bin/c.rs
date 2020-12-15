dababa47c7f45378ef17bad8fd5178c23a2b1b73
k-yomo <kanji.yy@gmail.com>
2020-12-16 00:22:32 +0900

    Solve C in abc067

--- rust/abc067/src/bin/c.rs
+++ rust/abc067/src/bin/c.rs
@@ -1,12 +1,20 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use itertools::__std_iter::once;
+use itertools_num::ItertoolsNum;
+use proconio::*;
+
+fn main() {
     input! {
-        
+        n: usize,
+        a: [i64; n],
     }
+
+    let a_acc = a.iter().cumsum().collect::<Vec<i64>>();
+    println!("{}", (0..n - 1).map(|i| (a_acc[i] - (a_acc[a_acc.len() - 1] - a_acc[i])).abs()).min().unwrap())
 }
