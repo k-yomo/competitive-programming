51495bb05a7afa6667e7ca1c01b405a521404630
k-yomo <kanji.yy@gmail.com>
2020-12-16 01:02:29 +0900

    Solve C in abc096

--- rust/abc096/src/bin/c.rs
+++ rust/abc096/src/bin/c.rs
@@ -1,12 +1,33 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use proconio::*;
+use proconio::marker::Chars;
+
+fn main() {
     input! {
-        
+        (h, w): (usize, usize),
+        s: [Chars; h],
     }
+
+    for y in 0..h {
+        for x in 0..w {
+            if s[y][x] != '#'
+                || y > 0 && s[y - 1][x] == '#'
+                || y < h - 1 && s[y + 1][x] == '#'
+                || x > 0 && s[y][x - 1] == '#'
+                || x < h - 1 && s[y][x + 1] == '#'
+            {
+                continue;
+            }
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
