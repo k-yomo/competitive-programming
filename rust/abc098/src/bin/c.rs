4fad897f7efa8b7439456cfaee37dcc5c8e53f68
k-yomo <kanji.yy@gmail.com>
2020-12-15 23:57:06 +0900

    Solve C in abc098

--- rust/abc098/src/bin/c.rs
+++ rust/abc098/src/bin/c.rs
@@ -1,3 +1,25 @@
+#![allow(unused_imports)]
+
+use std::cmp::*;
+use std::collections::*;
+use std::io::Write;
+use std::ops::Bound::*;
+
+use itertools::*;
+use proconio::*;
+use proconio::marker::*;
+use superslice::*;
+use itertools_num::ItertoolsNum;
+use itertools::__std_iter::once;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    let w_acc = once(0).chain(s.iter().map(|c| if *c == 'W' { 1 } else { 0 })).cumsum().collect::<Vec<usize>>();
+    let e_acc = once(0).chain(s.iter().map(|c| if *c == 'E' { 1 } else { 0 })).cumsum().collect::<Vec<usize>>();
+    println!("{}", s.iter().enumerate().map(|(i, c)| w_acc[i] + (e_acc[e_acc.len()-1] - e_acc[i+1])).min().unwrap());
 }
+
--- rust/abc098/src/bin/d.rs
+++ rust/abc098/src/bin/d.rs
@@ -1,3 +1,18 @@
+#![allow(unused_imports)]
+
+use std::cmp::*;
+use std::collections::*;
+use std::io::Write;
+use std::ops::Bound::*;
+
+use itertools::*;
+use proconio::*;
+use proconio::marker::*;
+use superslice::*;
+
 fn main() {
-    todo!();
+    input! {
+    }
+
 }
+
