e2ec46739d33c171a24da991f7f1c03d041e0b07
k-yomo <kanji.yy@gmail.com>
2020-12-16 21:10:07 +0900

    Solve C in abc094

--- rust/abc094/src/bin/c.rs
+++ rust/abc094/src/bin/c.rs
@@ -4,9 +4,18 @@ use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+fn main() {
     input! {
-        
+        n: usize,
+        x: [usize; n],
+    }
+
+    let mut sorted_x = x.clone();
+    sorted_x.sort();
+    let med1 = sorted_x[n / 2 - 1];
+    let med2 = sorted_x[n / 2];
+    for num in x {
+        println!("{}", if num <= med1 { med2 } else { med1 });
     }
 }
