497d034a6d711e88d6e0ab10fae6deb464468231
k-yomo <kanji.yy@gmail.com>
2020-12-16 20:48:18 +0900

    Solve C in abc092

--- rust/abc092/src/bin/c.rs
+++ rust/abc092/src/bin/c.rs
@@ -1,12 +1,25 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use itertools::__std_iter::once;
+use proconio::*;
+
+fn main() {
     input! {
-        
+        n: usize,
+        mut a: [i64; n],
+    }
+
+    a.insert(0, 0);
+    a.push(0);
+    let cost = a.windows(2).map(|w| (w[1] - w[0]).abs()).sum::<i64>();
+    for i in 1..n+1 {
+        let last = a[i - 1];
+        let next = a[i + 1];
+        println!("{}", cost + (next - last).abs() - (a[i] - last).abs() - (next - a[i]).abs())
     }
 }
