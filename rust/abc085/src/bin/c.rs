c6d93911135f23ba359fcd4723ceb752607d3310
k-yomo <kanji.yy@gmail.com>
2020-12-18 00:42:42 +0900

    Solve C in abc085

--- rust/abc085/src/bin/c.rs
+++ rust/abc085/src/bin/c.rs
@@ -8,6 +8,19 @@ use std::ops::Bound::*;
  
 fn main() { 
     input! {
-        
+        (n, y): (usize, usize),
     }
+    for i in 0..20001 {
+        if 10000 * i > y {
+            break
+        }
+        for j in 0..n-i+1 {
+            if 10000 * i + 5000 * j + 1000 * (n-i-j) == y {
+                println!("{} {} {}", i, j, (n-i-j));
+                return;
+            }
+        }
+    }
+
+    println!("-1 -1 -1");
 }
