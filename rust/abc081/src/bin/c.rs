72f878abe8bb1550968b8a6ca4d21d18167ec842
k-yomo <kanji.yy@gmail.com>
2020-12-17 22:00:12 +0900

    Solve C in abc081

--- rust/abc081/src/bin/c.rs
+++ rust/abc081/src/bin/c.rs
@@ -8,6 +8,29 @@ use std::ops::Bound::*;
  
 fn main() { 
     input! {
-        
+        n: usize,
+        a: [usize; n],
     }
+
+    let mut num_map = HashMap::new();
+    let mut first = 0;
+    let mut second = 0;
+    for num in a {
+        let count: &mut i64 = num_map.entry(num).or_insert(0);
+        *count += 1;
+        if *count < 2 {
+            continue
+        }
+        if num == first || num == second {
+            continue
+        }
+        if second < num {
+            first = second;
+            second = num;
+        } else if first < num {
+            first = num;
+        }
+    }
+    println!("first: {}, second: {}", first, second);
+    println!("{}", first * second);
 }
