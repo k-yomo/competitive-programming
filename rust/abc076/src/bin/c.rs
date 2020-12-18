ef9a4aa8cdc20ea6de54f0d6cf097bb09512129e
k-yomo <kanji.yy@gmail.com>
2020-12-19 02:22:20 +0900

    Solve C in abc076

--- rust/abc076/src/bin/c.rs
+++ rust/abc076/src/bin/c.rs
@@ -1,13 +1,36 @@
 #![allow(unused_imports)]
-use proconio::*;
-use proconio::marker::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use proconio::*;
+use proconio::marker::*;
+
+fn main() {
     input! {
-        
+        mut s: Chars,
+        t: Chars,
     }
+
+    if t.len() > s.len() {
+        println!("UNRESTORABLE");
+        return;
+    }
+
+    let mut is_replaced = false;
+    let diff = s.len() - (t.len() - 1);
+    for i in (0..diff).rev() {
+        if (i..(i + t.len())).enumerate().all(|(i, j)| s[j] == '?' || s[j] == t[i]) {
+            (i..(i + t.len())).enumerate().for_each(|(i, j)| s[j] = t[i]);
+            is_replaced = true;
+            break;
+        }
+    }
+    if !is_replaced {
+        println!("UNRESTORABLE");
+        return;
+    }
+    println!("{}", s.iter().map(|&c| if c == '?' { 'a' } else { c }).collect::<String>());
 }
