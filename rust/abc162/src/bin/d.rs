0d270d4391f8f0b4f2f48e06b08e5271728c6720
k-yomo <kanji.yy@gmail.com>
2020-12-19 17:47:31 +0900

    Solve D in arc162

--- rust/abc162/src/bin/d.rs
+++ rust/abc162/src/bin/d.rs
@@ -1,14 +1,34 @@
 #![allow(unused_imports)]
-use proconio::*;
-use proconio::marker::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
 
-fn main() { 
+use itertools::*;
+use proconio::*;
+use proconio::marker::*;
+
+fn main() {
     input! {
-        
+        n: usize,
+        s: Chars,
     }
+
+    let r = s.iter().filter(|&&c| c == 'R').count();
+    let g = s.iter().filter(|&&c| c == 'G').count();
+    let b = s.iter().filter(|&&c| c == 'B').count();
+    let all = r * g * b;
+
+    let mut sub = 0;
+    for i in 0..n {
+        for j in i+1..n {
+            let k = j * 2 - i;
+            if k >= n || s[i] == s[j] || s[i] == s[k] || s[j] == s[k] {
+                continue
+            }
+            sub += 1;
+        }
+    }
+    println!("{:?}", all - sub);
 }
