8b540df4fcfd4d84e35de24492ed446bae974655
k-yomo <kanji.yy@gmail.com>
2020-12-19 18:30:16 +0900

    Solve D in abc180

--- rust/abc180/src/bin/d.rs
+++ rust/abc180/src/bin/d.rs
@@ -1,14 +1,23 @@
 #![allow(unused_imports)]
-use proconio::*;
-use proconio::marker::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
 
-fn main() { 
+use itertools::*;
+use proconio::*;
+use proconio::marker::*;
+
+fn main() {
     input! {
-        
+        (mut x, y, a, b): (u128, u128, u128, u128),
     }
+
+    let mut exp = 0;
+    while x * a <= x + b && x * a < y {
+        x *= a;
+        exp += 1;
+    }
+    println!("{}", exp + (y - 1 - x) / b);
 }
