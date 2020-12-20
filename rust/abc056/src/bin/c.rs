99fce73723c41a6ef2fa28cd33649bae3706ff92
k-yomo <kanji.yy@gmail.com>
2020-12-20 16:39:47 +0900

    Solve C in abc056

--- rust/abc056/src/bin/c.rs
+++ rust/abc056/src/bin/c.rs
@@ -1,12 +1,25 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use itertools::*;
+use proconio::*;
+use proconio::marker::*;
+use superslice::*;
+
+fn main() {
     input! {
-        
+        x: usize,
+    }
+    let mut sum = 0;
+    for i in 1..10_usize.pow(9) {
+        sum += i;
+        if sum >= x {
+            println!("{}", i);
+            return
+        }
     }
 }
