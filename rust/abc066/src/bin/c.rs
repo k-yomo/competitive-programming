f5e1d970513204617bb38e163a756224145af773
k-yomo <kanji.yy@gmail.com>
2020-12-25 04:55:23 +0900

    Solve C in abc066

--- rust/abc066/src/bin/c.rs
+++ rust/abc066/src/bin/c.rs
@@ -1,12 +1,25 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+use proconio::*;
+
+fn main() {
     input! {
-        
+        n: usize,
+        a: [usize; n],
     }
+
+    let mut ans = VecDeque::with_capacity(n);
+    for (i, &num) in a.iter().enumerate() {
+        if (n-1) % 2 == i % 2 {
+            ans.push_front(num.to_string())
+        } else {
+            ans.push_back(num.to_string())
+        }
+    }
+
+    println!("{}", ans.into_iter().collect::<Vec<_>>().join(" "));
 }
