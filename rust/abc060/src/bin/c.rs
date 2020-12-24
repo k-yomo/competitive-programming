6dfeaef561f3555d0cb633ed9c4d3aee705c5ec1
k-yomo <kanji.yy@gmail.com>
2020-12-25 03:39:23 +0900

    Solve C in abc060

--- rust/abc060/src/bin/c.rs
+++ rust/abc060/src/bin/c.rs
@@ -1,12 +1,26 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use proconio::*;
+
+fn main() {
     input! {
-        
+        (n, t): (usize, usize),
+        ts: [usize; n],
     }
+
+    let mut total = t;
+    for (i, &cur_time) in ts[1..].iter().enumerate() {
+        if cur_time >= ts[i] + t {
+            total += t;
+        } else {
+            total += cur_time - ts[i];
+        }
+    }
+
+    println!("{}", total);
 }
