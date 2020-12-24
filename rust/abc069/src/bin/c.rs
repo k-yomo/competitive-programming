d6bffbf4369add84b891a35da6f1110c6209550d
k-yomo <kanji.yy@gmail.com>
2020-12-25 03:23:18 +0900

    Solve C in abc069

--- rust/abc069/src/bin/c.rs
+++ rust/abc069/src/bin/c.rs
@@ -1,12 +1,30 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use proconio::*;
+
+fn main() {
     input! {
-        
+        mut n: usize,
+        a: [usize; n],
     }
+
+    let (mut four_mul, mut two_mul, mut other): (usize, usize, usize) = (0, 0, 0);
+    for num in a {
+        if num % 4 == 0 {
+            four_mul += 1;
+        } else if num % 2 == 0 {
+            two_mul += 1;
+        } else {
+            other += 1;
+        }
+    }
+    if two_mul > 0 {
+        other += 1;
+    }
+    println!("{}", if four_mul + 1 >= other { "Yes" } else { "No" });
 }
