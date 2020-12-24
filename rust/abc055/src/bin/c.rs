d3acf2aeca70eb43e3bd4984b488199fe110f3e6
k-yomo <kanji.yy@gmail.com>
2020-12-25 05:02:34 +0900

    Solve C in abc055

--- rust/abc055/src/bin/c.rs
+++ rust/abc055/src/bin/c.rs
@@ -7,6 +7,13 @@ use std::ops::Bound::*;
  
 fn main() { 
     input! {
-        
+        (n, m): (usize, usize),
     }
+
+    if n >= m/2 {
+        println!("{}", m/2);
+        return
+    }
+
+    println!("{}", n + ((m - n * 2) / 4));
 }
