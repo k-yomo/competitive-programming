d9aff00e9b662dd624a00abd76ba04014f771e99
k-yomo <kanji.yy@gmail.com>
2020-12-27 04:35:24 +0900

    Solve C in abc064

--- rust/abc064/src/bin/c.rs
+++ rust/abc064/src/bin/c.rs
@@ -1,12 +1,23 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use proconio::*;
+
+fn main() {
     input! {
-        
+        n: usize,
+        a: [usize; n],
     }
+
+    let mut atcoder_colors = vec![0; 9];
+    for rate in a {
+        atcoder_colors[std::cmp::min(rate / 400, 8)] += 1;
+    }
+
+    let color_count = atcoder_colors[0..8].iter().filter(|&&num| num > 0).count();
+    println!("{} {}", std::cmp::max(color_count, 1), color_count + atcoder_colors[8]);
 }
