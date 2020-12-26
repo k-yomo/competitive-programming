43a250b94f004086203454a6a60eae6a542dba86
k-yomo <kanji.yy@gmail.com>
2020-12-27 03:58:48 +0900

    Solve C in abc053

--- rust/abc053/src/bin/c.rs
+++ rust/abc053/src/bin/c.rs
@@ -7,6 +7,14 @@ use std::ops::Bound::*;
  
 fn main() { 
     input! {
-        
+        mut x: usize,
     }
+
+    let mut ans = x / 11 * 2;
+    x %= 11;
+    if x > 0 {
+        ans += if x > 6  { 2 } else { 1 };
+    }
+
+    println!("{}", ans);
 }
