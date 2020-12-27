6e6219c24957bfa8ac21428b917cd1878b6bd301
k-yomo <kanji.yy@gmail.com>
2020-12-27 14:44:43 +0900

    Solve C in abc068

--- rust/abc068/src/bin/c.rs
+++ rust/abc068/src/bin/c.rs
@@ -1,12 +1,26 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use proconio::*;
+
+fn main() {
     input! {
-        
+        (n, m): (usize, usize),
+        ab: [(usize, usize); m],
     }
+
+    let mut island_map = HashMap::new();
+    ab.iter().filter(|(a, _)| *a == 1).for_each(|(_, b)| { island_map.insert(*b, true); });
+    for (a, b) in ab {
+        if *island_map.entry(a).or_default() && b == n || a == n && *island_map.entry(b).or_default() {
+            println!("POSSIBLE");
+            return;
+        }
+    }
+
+    println!("IMPOSSIBLE");
 }
