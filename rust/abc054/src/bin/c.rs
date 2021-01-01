52fd5ac99e1f6e8c79365b40036f4a57112c29ff
k-yomo <kanji.yy@gmail.com>
2021-01-01 14:20:24 +0900

    Solve C in abc054

--- rust/abc054/src/bin/c.rs
+++ rust/abc054/src/bin/c.rs
@@ -5,9 +5,30 @@ use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+use itertools::*;
+
+fn main() {
     input! {
-        
+        (n, m): (usize, usize),
+        ab: [(usize, usize); m],
     }
+
+    let mut comb_count = 0;
+    for mut comb in (2..n + 1).permutations(n-1) {
+        let mut perm = vec![1];
+        perm.append(&mut comb);
+        let is_valid = perm.windows(2).all(|w| {
+            for (a, b) in ab.iter() {
+                if a == &w[0] && b == &w[1] || a == &w[1] && b == &w[0] {
+                    return true
+                }
+            }
+            false
+        });
+        if is_valid {
+            comb_count += 1;
+        }
+    }
+
+    println!("{}", comb_count);
 }
