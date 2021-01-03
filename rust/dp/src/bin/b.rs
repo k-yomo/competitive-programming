ff16e81f283a6180d67931b7cdbc9cee84b32d53
k-yomo <kanji.yy@gmail.com>
2021-01-03 14:38:57 +0900

    Solve B in dp

--- rust/dp/src/bin/b.rs
+++ rust/dp/src/bin/b.rs
@@ -10,8 +10,23 @@ use proconio::*;
 use proconio::marker::*;
 use superslice::*;
 
-fn main() { 
+fn main() {
     input! {
-        
+        (n, k): (usize, usize),
+        h: [i64; n],
     }
+
+    let mut dp = vec![0; n];
+    dp[1] = (h[1] - h[0]).abs();
+    for i in 2..n {
+        let mut min_step = std::i64::MAX;
+        for j in 1..k + 1 {
+            if i >= j {
+                min_step = std::cmp::min(min_step, dp[i - j] + (h[i] - h[i - j]).abs());
+            }
+        }
+        dp[i] = min_step;
+    }
+
+    println!("{}", dp[n - 1]);
 }
