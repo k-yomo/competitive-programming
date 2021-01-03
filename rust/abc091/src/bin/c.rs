4a4dbf8519817a90f13b264b6f5d3688d6d31d7f
k-yomo <kanji.yy@gmail.com>
2021-01-03 11:51:30 +0900

    Solve C in abc091

--- rust/abc091/src/bin/c.rs
+++ rust/abc091/src/bin/c.rs
@@ -1,12 +1,32 @@
 #![allow(unused_imports)]
-use proconio::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use proconio::*;
+
+fn main() {
     input! {
-        
+        n: usize,
+        mut red: [(usize, usize); n],
+        mut blue: [(usize, usize); n],
     }
+
+    red.sort_by_key(|x| x.1);
+    blue.sort_by_key(|x| x.0);
+
+    let mut match_count = 0;
+    for (blue_x, blue_y) in blue {
+        for (i, (red_x, red_y)) in red.iter().rev().enumerate() {
+            if *red_x < blue_x && *red_y < blue_y {
+                match_count += 1;
+                red.remove(red.len() - 1 - i);
+                break;
+            }
+        }
+    }
+
+    println!("{}", match_count);
 }
