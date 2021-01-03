9af1487de26e0888e7d21962ebea1c5c199734eb
k-yomo <kanji.yy@gmail.com>
2021-01-03 15:24:53 +0900

    Solve D in abc052

--- rust/abc052/src/bin/d.rs
+++ rust/abc052/src/bin/d.rs
@@ -7,6 +7,15 @@ use std::ops::Bound::*;
  
 fn main() { 
     input! {
-        
+        (n, a, b): (usize, usize, usize),
+        x: [usize; n],
     }
+
+
+    let mut dp = vec![0; n];
+    for i in 1..n {
+        dp[i] = dp[i - 1] + std::cmp::min( (x[i] - x[i -1]) * a, b);
+    }
+
+    println!("{}", dp[n - 1]);
 }
