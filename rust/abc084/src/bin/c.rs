0d91eda800ce199c4d399814a04172f902c45a3e
k-yomo <kanji.yy@gmail.com>
2021-01-05 00:41:39 +0900

    Solve C in abc084

--- rust/abc084/src/bin/c.rs
+++ rust/abc084/src/bin/c.rs
@@ -5,9 +5,23 @@ use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+fn main() {
     input! {
-        
+        n: usize,
+        (csf): [(usize, usize, usize); n-1],
     }
+
+    for i in 0..(n - 1) {
+        let mut elapsed_sec = csf[i].0 + csf[i].1;
+        for j in (i+1)..(n - 1) {
+            elapsed_sec += csf[j].0 + if elapsed_sec <= csf[j].1 {
+                csf[j].1 - elapsed_sec
+            } else {
+               (csf[j].2 - (elapsed_sec - csf[j].1) % csf[j].2 ) % csf[j].2
+            }
+        }
+        println!("{}", elapsed_sec);
+    }
+    println!("0");
 }
