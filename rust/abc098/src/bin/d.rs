66aa9719394c0c734f7c30c0a3692a3ad1f0902b
k-yomo <kanji.yy@gmail.com>
2021-01-06 04:00:46 +0900

    Solve D in abc098

--- rust/abc098/src/bin/d.rs
+++ rust/abc098/src/bin/d.rs
@@ -12,7 +12,26 @@ use superslice::*;
 
 fn main() {
     input! {
+        n: usize,
+        a: [usize; n],
     }
 
+    let mut count = 0_usize;
+    let (mut l, mut r, mut xor, mut sum) = (0, 0, 0, 0);
+    while l < n {
+        while r < n && sum ^ a[r] == sum + a[r] {
+            sum += a[r];
+            r += 1;
+        }
+        count += r - l;
+        if l == r {
+            r += 1;
+        } else {
+            sum -= a[l];
+        }
+        l += 1;
+    }
+
+    println!("{}", count);
 }
 
