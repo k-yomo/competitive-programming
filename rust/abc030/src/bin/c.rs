6208765069e437e90373c9879b58f1b9733142da
k-yomo <kanji.yy@gmail.com>
2021-01-05 21:09:06 +0900

    Solve C in abc030

--- rust/abc030/src/bin/c.rs
+++ rust/abc030/src/bin/c.rs
@@ -1,13 +1,32 @@
 #![allow(unused_imports)]
-use proconio::*;
-use proconio::marker::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use proconio::*;
+use proconio::marker::*;
+use superslice::*;
+
+fn main() {
     input! {
-        
+        (n, m): (usize, usize),
+        (x, y): (usize, usize),
+        a: [usize; n],
+        b: [usize; m],
     }
+
+    let (mut ai, mut bi) = (0_usize, 0_usize);
+    let mut roundtrip_count = 0;
+    while ai < n && bi < m {
+        bi = b.lower_bound(&(a[ai] + x));
+        if bi >= m {
+            break;
+        }
+        ai = a.lower_bound(&(b[bi] + y));
+        roundtrip_count += 1;
+    }
+
+    println!("{}", roundtrip_count);
 }
