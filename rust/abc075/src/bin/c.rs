32313b61fce5c9ac8af316257f6d20c6dee2083d
k-yomo <kanji.yy@gmail.com>
2021-01-06 21:47:28 +0900

    Solve C in abc075

--- rust/abc075/src/bin/c.rs
+++ rust/abc075/src/bin/c.rs
@@ -1,13 +1,32 @@
 #![allow(unused_imports)]
-use proconio::*;
-use proconio::marker::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+use petgraph::unionfind::UnionFind;
+use proconio::*;
+use proconio::marker::*;
+
+fn main() {
     input! {
-        
+        (n, m): (usize, usize),
+        ab: [(usize, usize); m],
     }
+
+    let mut bridge_count = 0;
+    for (i, (a, b)) in ab.iter().enumerate() {
+        let mut uf = UnionFind::new(n);
+        for (j, (a, b)) in ab.iter().enumerate() {
+            if i != j {
+                uf.union(a - 1, b - 1);
+            }
+        }
+        if !uf.equiv(a - 1, b - 1) {
+            bridge_count += 1;
+        }
+    }
+
+    println!("{}", bridge_count);
 }
