cb65c6cd9c4bf7e3a07c183fc781a252ff2c08ca
k-yomo <kanji.yy@gmail.com>
2021-01-06 22:26:37 +0900

    Solve C in abc057

--- rust/abc057/src/bin/c.rs
+++ rust/abc057/src/bin/c.rs
@@ -7,6 +7,17 @@ use std::ops::Bound::*;
  
 fn main() { 
     input! {
-        
+        n: usize,
     }
+    
+    let mut min_digit = n;
+    let mut i = 1;
+    while i * i <= n {
+        if n % i == 0 {
+            let digit_len = std::cmp::max(i, n / i).to_string().chars().map(|d| d.to_digit(10).unwrap()).collect::<Vec<_>>().len();
+            min_digit = std::cmp::min(min_digit, digit_len);
+        }
+        i += 1;
+    }
+    println!("{}", min_digit);
 }
