a8f6bd2db63c52ce92b1f3a4f66315238e340a4c
k-yomo <kanji.yy@gmail.com>
2021-01-07 21:39:05 +0900

    Solve D in abc182

--- rust/abc182/src/bin/d.rs
+++ rust/abc182/src/bin/d.rs
@@ -1,17 +1,32 @@
 #![allow(unused_imports)]
-use proconio::*;
-use proconio::marker::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
 
-fn main() { 
+use itertools::*;
+use itertools::__std_iter::once;
+use itertools_num::ItertoolsNum;
+use proconio::*;
+use proconio::marker::*;
+
+fn main() {
     input! {
         n: usize,
-        a: [i64; n],
+        a: [i128; n],
     }
 
+    let mut total_diff = 0;
+    let mut max_diff = 0;
+    let mut cur_x = 0;
+    let mut max = 0;
+    for x in a {
+        total_diff += x;
+        max_diff = std::cmp::max(max_diff, total_diff);
+        max = std::cmp::max(max, cur_x + max_diff);
+        cur_x += total_diff;
+    }
 
+    println!("{}", max);
 }
