1b70d0724d59c0785712e5fa5b8ce51212a6c800
k-yomo <kanji.yy@gmail.com>
2021-01-09 05:58:02 +0900

    Solve D in abc181

--- rust/abc181/src/bin/d.rs
+++ rust/abc181/src/bin/d.rs
@@ -1,16 +1,50 @@
 #![allow(unused_imports)]
-use proconio::*;
-use proconio::marker::*;
+
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
 
-fn main() { 
+use itertools::*;
+use proconio::*;
+use proconio::marker::*;
+use whiteread::parse_string;
+
+fn main() {
     input! {
-        s: usize,
+        s: Chars,
     }
 
+    if s.len() <= 2 {
+        return if parse_string::<usize>(&s.iter().collect::<String>()).unwrap() % 8 == 0 ||
+            parse_string::<usize>(&s.iter().rev().collect::<String>()).unwrap() % 8 == 0 {
+            println!("Yes")
+        } else {
+            println!("No")
+        };
+    }
 
+    let mut num_counts = vec![0; 10];
+    for c in s {
+        num_counts[(c as u8 - '0' as u8) as usize] += 1;
+    }
+
+    for i in (112..1000).step_by(8) {
+        let a = i / 100;
+        let b = (i / 10) % 10;
+        let c = i % 10;
+        if a == 0 || b == 0 || c == 0 {
+            continue;
+        }
+        num_counts[a] -= 1;
+        num_counts[b] -= 1;
+        num_counts[c] -= 1;
+        if num_counts[a] >= 0 && num_counts[b] >= 0 && num_counts[c] >= 0 {
+            return println!("Yes");
+        }
+        num_counts[a] += 1;
+        num_counts[b] += 1;
+        num_counts[c] += 1;
+    }
+    println!("No");
 }
