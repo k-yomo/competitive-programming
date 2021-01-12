5af26216b96d4c4d367285fad4c248b2adfd5844
k-yomo <kanji.yy@gmail.com>
2021-01-13 00:59:27 +0900

    Solve B in dp

--- rust/dp/src/bin/d.rs
+++ rust/dp/src/bin/d.rs
@@ -10,8 +10,20 @@ use proconio::*;
 use proconio::marker::*;
 use superslice::*;
 
-fn main() { 
+fn main() {
     input! {
-        
+        n: usize, W: usize,
+        wv: [(usize, usize); n],
     }
+
+    let mut dp = vec![vec![0; W + 1]; n + 1];
+    for (i, w) in iproduct!(0..n, 0..W+1) {
+        if w < wv[i].0 {
+            dp[i + 1][w] = dp[i][w];
+        } else {
+            dp[i + 1][w] = std::cmp::max(dp[i][w - wv[i].0] + wv[i].1, dp[i][w]);
+        }
+    }
+
+    println!("{}", dp[n][W]);
 }
