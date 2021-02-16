1df8a5f16105cefdfffcf5f1a32b5a3e7b480025
k-yomo <kanji.yy@gmail.com>
2021-02-17 00:03:35 +0900

    Solve B in abc191

--- rust/abc191/src/bin/b.rs
+++ rust/abc191/src/bin/b.rs
@@ -5,15 +5,17 @@ use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
 
-use itertools::*;
 use itertools::__std_iter::once;
+use itertools::*;
 use itertools_num::ItertoolsNum;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
-fn main() { 
+fn main() {
     input! {
-        
+        n: usize, x: String,
+        a: [String; n]
     }
+    println!("{}", a.into_iter().filter(|i| *i != x).join(" "));
 }
