55afeac6c60adb3d683c16a87f9a7846e332db26
k-yomo <kanji.yy@gmail.com>
2021-02-25 20:20:36 +0900

    Solve D in abc125

--- rust/abc125/src/bin/d.rs
+++ rust/abc125/src/bin/d.rs
@@ -6,12 +6,23 @@ use std::io::Write;
 use std::ops::Bound::*;
 
 use itertools::*;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
-fn main() { 
+fn main() {
     input! {
-        
+        n: usize,
+        mut a: [i128; n],
+    }
+
+    let mut minus_count = a.iter().filter(|x| **x <= 0).count();
+    if minus_count % 2 == 0 {
+        println!("{}", a.iter().map(|x| x.abs()).sum::<i128>());
+    } else {
+        println!(
+            "{}",
+            a.iter().map(|x| x.abs()).sum::<i128>() - a.iter().map(|x| x.abs()).min().unwrap() * 2
+        );
     }
 }
