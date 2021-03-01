917e6c0d31323511b6d0ebad8a1219bd887474e0
k-yomo <kanji.yy@gmail.com>
2021-03-01 21:06:37 +0900

    Solve C in abc191

--- atcoder/rust/abc191/src/bin/c.rs
+++ atcoder/rust/abc191/src/bin/c.rs
@@ -14,6 +14,19 @@ use superslice::*;
 
 fn main() { 
     input! {
-        
+        h: usize, w: usize,
+        s: [Chars; h]
     }
+
+    let mut corner_count = 0;
+    for y in 1..h {
+        for x in 1..w {
+            let mut black_num = vec![s[y][x], s[y][x-1], s[y-1][x-1], s[y-1][x]].iter().filter(|x| **x == '#').count();
+            if black_num == 1 || black_num == 3 {
+                corner_count += 1;
+            }
+        }
+    }
+
+    println!("{}", corner_count);
 }
