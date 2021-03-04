70f596a298eb5e373ae2d76dffc15f1f47827cce
k-yomo <kanji.yy@gmail.com>
2021-03-05 00:01:46 +0900

    Solve D in abc174

--- atcoder/rust/abc174/src/bin/d.rs
+++ atcoder/rust/abc174/src/bin/d.rs
@@ -6,14 +6,20 @@ use std::io::Write;
 use std::ops::Bound::*;
 
 use itertools::*;
-use itertools_num::ItertoolsNum;
 use itertools::__std_iter::once;
+use itertools_num::ItertoolsNum;
 use proconio::*;
 use proconio::marker::*;
 use superslice::*;
 
-fn main() { 
+fn main() {
     input! {
-        
+        n: usize,
+        c: Chars,
     }
+
+    let a_list = c.into_iter().map(|color| if color == 'R' { 1 } else { 0 }).collect::<Vec<usize>>();
+    let r_count = a_list.iter().sum::<usize>();
+
+    println!("{}", r_count - a_list[0..r_count].iter().sum::<usize>())
 }
