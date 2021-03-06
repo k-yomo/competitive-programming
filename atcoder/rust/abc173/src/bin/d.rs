19ae07ba88a2b03432977a5c058d1bd53d565531
k-yomo <kanji.yy@gmail.com>
2021-03-06 19:17:29 +0900

    Solve D in abc173

--- atcoder/rust/abc173/src/bin/d.rs
+++ atcoder/rust/abc173/src/bin/d.rs
@@ -1,13 +1,28 @@
 #![allow(unused_imports)]
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+fn main() {
     input! {
-        
+        n: usize,
+        mut a: [usize; n],
     }
+
+    a.sort();
+    a.reverse();
+
+    let mut sum = 0;
+    let mut que = VecDeque::new();
+    que.push_back(a[0]);
+    for i in 1..n {
+        sum += que.pop_front().unwrap();
+        que.push_back(a[i]);
+        que.push_back(a[i]);
+    }
+
+    println!("{}", sum);
 }
