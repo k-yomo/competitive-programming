41c19e37bf8ed25a99c47a6c58782f315c4d8e7c
k-yomo <kanji.yy@gmail.com>
2021-03-09 13:34:15 +0900

    Solve C in abc026

--- atcoder/rust/abc026/src/bin/c.rs
+++ atcoder/rust/abc026/src/bin/c.rs
@@ -6,12 +6,31 @@ use std::io::Write;
 use std::ops::Bound::*;
 
 use itertools::*;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
 fn main() {
     input! {
-
+        n: usize,
+    mut b: [usize; n-1],
     }
+
+    let mut members = vec![vec![]; n];
+    for (i, b) in b.iter().enumerate() {
+        members[b - 1].push(i + 1);
+    }
+
+    println!("{}", calc_salary(&members, 0));
+}
+
+fn calc_salary(members: &Vec<Vec<usize>>, i: usize) -> usize {
+    if members[i].len() == 0 {
+        return 1;
+    }
+    let mut salaries = members[i]
+        .iter()
+        .map(|m| calc_salary(&members, *m))
+        .collect::<Vec<usize>>();
+    return salaries.iter().max().unwrap() + salaries.iter().min().unwrap() + 1;
 }
