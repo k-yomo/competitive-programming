779a44eb157b5e51ad5f803a0a0657163dcb6abf
k-yomo <kanji.yy@gmail.com>
2021-03-11 19:25:21 +0900

    Solve D in abc141

--- atcoder/rust/abc141/src/bin/d.rs
+++ atcoder/rust/abc141/src/bin/d.rs
@@ -5,13 +5,28 @@ use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
 
+use itertools::__std_iter::once;
 use itertools::*;
-use proconio::*;
+use itertools_num::ItertoolsNum;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
-fn main() { 
+fn main() {
     input! {
-        
+        n: usize, m: usize,
+        mut a: [usize; n],
     }
+
+    let mut bh = BinaryHeap::new();
+    for i in a {
+        bh.push(i);
+    }
+
+    for _ in 0..m {
+        let price = bh.pop().unwrap();
+        bh.push(price / 2);
+    }
+
+    println!("{}", bh.into_sorted_vec().iter().sum::<usize>());
 }
