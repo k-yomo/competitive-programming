76717412ed47bf51edb9f7accd422023ec2189c4
k-yomo <kanji.yy@gmail.com>
2021-03-25 20:45:58 +0900

    Solve E in abc153

---
+++ atcoder/rust/abc153/src/bin/e.rs
@@ -0,0 +1,38 @@
+#![allow(unused_imports)]
+
+use std::cmp::*;
+use std::collections::*;
+use std::io::Write;
+use std::ops::Bound::*;
+
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
+use superslice::*;
+
+#[macro_export]
+macro_rules! chmin {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_min = min ! ($ ($ cmps ) ,+ ) ; if $ base > cmp_min {$ base = cmp_min ; true } else {false } } } ; }
+#[macro_export]
+macro_rules! chmax {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_max = max ! ($ ($ cmps ) ,+ ) ; if $ base < cmp_max {$ base = cmp_max ; true } else {false } } } ; }
+#[macro_export]
+macro_rules! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: min ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: min ($ a , min ! ($ ($ rest ) ,+ ) ) } } ; }
+#[macro_export]
+macro_rules! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
+
+fn main() {
+    input! {
+        H: usize, n: usize,
+        ab: [(usize, usize); n],
+    }
+
+    // dp[h] = 減らす体力が h 以上になるように選んだときの魔力の合計の最小値
+    let mut dp = vec![10_usize.pow(8); H + 1];
+    dp[0] = 0;
+    for (i, h) in iproduct!(0..n, 0..=H) {
+        chmin!(dp[std::cmp::min(ab[i].0 + h, H)], dp[h] + ab[i].1);
+    }
+
+    println!("{}", dp[H]);
+}
