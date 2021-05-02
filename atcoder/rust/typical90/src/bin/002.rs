594c04256a325cf4cf92a3330c943637e511ea14
k-yomo <kanji.yy@gmail.com>
2021-05-02 18:36:42 +0900

    Solve 002 in typical90

--- atcoder/rust/typical90/src/bin/002.rs
+++ atcoder/rust/typical90/src/bin/002.rs
@@ -5,11 +5,11 @@ use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
 
-use itertools::*;
 use itertools::__std_iter::once;
+use itertools::*;
 use itertools_num::ItertoolsNum;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
 #[macro_export]
@@ -21,8 +21,42 @@ macro_rules! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr
 #[macro_export]
 macro_rules! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
 
-fn main() { 
+fn main() {
     input! {
-        
+        mut n: usize,
+    }
+
+    if n % 2 != 0 {
+        return;
+    }
+
+    let mut parentheses = vec![];
+    let mut queue = vec![];
+    queue.push((vec!['('], 1));
+    while queue.len() > 0 {
+        let (mut p, unclosed_count) = queue.pop().unwrap();
+        if p.len() == n {
+            parentheses.push(p);
+            continue;
+        }
+        if unclosed_count == n - p.len() {
+            for _ in 0..unclosed_count {
+                p.push(')');
+            }
+            parentheses.push(p);
+        } else {
+            let mut a = p.clone();
+            a.push('(');
+            queue.push((a, unclosed_count + 1));
+            if unclosed_count > 0 {
+                let mut b = p.clone();
+                b.push(')');
+                queue.push((b, unclosed_count - 1));
+            }
+        }
+    }
+
+    for p in parentheses.iter().rev() {
+        println!("{}", p.iter().collect::<String>());
     }
 }
