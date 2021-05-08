3729e54869e4ffc3b026d4ba1ea696b14bebbb20
k-yomo <kanji.yy@gmail.com>
2021-05-09 00:43:06 +0900

    Sole A,B,C in abc199

---
+++ atcoder/rust/abc199/src/bin/b.rs
@@ -0,0 +1,39 @@
+#![allow(unused_imports)]
+
+use std::cmp::*;
+use std::collections::*;
+use std::io::Write;
+use std::ops::Bound::*;
+
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
+use superslice::*;
+
+#[macro_export]
+macro_rules! chmin {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_min = min ! ($ ($ cmps ) ,+ ) ; if $ base > cmp_min {$ base = cmp_min ; true } else {false } } } ; }
+#[macro_export]
+macro_rules! chmax {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_max = max ! ($ ($ cmps ) ,+ ) ; if $ base < cmp_max {$ base = cmp_max ; true } else {false } } } ; }
+#[macro_export]
+macro_rules! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: min ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: min ($ a , min ! ($ ($ rest ) ,+ ) ) } } ; }
+#[macro_export]
+macro_rules! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
+
+fn main() {
+    input! {
+        n: usize,
+        a: [usize; n],
+        b: [usize; n],
+    }
+
+    let mut min = 0;
+    let mut max = 1001;
+    for i in 0..n {
+        min = std::cmp::max(min, a[i]);
+        max = std::cmp::min(max, b[i]);
+    }
+
+    println!("{}", if max >= min { max + 1 - min } else { 0 })
+}
---
+++ atcoder/rust/abc199/src/bin/c.rs
@@ -0,0 +1,56 @@
+#![allow(unused_imports)]
+
+use std::cmp::*;
+use std::collections::*;
+use std::io::Write;
+use std::ops::Bound::*;
+
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
+use superslice::*;
+
+#[macro_export]
+macro_rules! chmin {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_min = min ! ($ ($ cmps ) ,+ ) ; if $ base > cmp_min {$ base = cmp_min ; true } else {false } } } ; }
+#[macro_export]
+macro_rules! chmax {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_max = max ! ($ ($ cmps ) ,+ ) ; if $ base < cmp_max {$ base = cmp_max ; true } else {false } } } ; }
+#[macro_export]
+macro_rules! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: min ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: min ($ a , min ! ($ ($ rest ) ,+ ) ) } } ; }
+#[macro_export]
+macro_rules! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
+
+fn main() {
+    input! {
+        n: usize,
+        mut s: Chars,
+        q: usize,
+        tab: [(usize, usize, usize); q],
+    }
+
+    let mut is_reverse = false;
+    for (t, a, b) in tab {
+        if t == 1 {
+            if !is_reverse {
+                s.swap(a - 1, b - 1);
+            } else {
+                let x = if a <= n { a + n } else { a - n };
+                let y = if b <= n { b + n } else { b - n };
+                s.swap(x - 1, y - 1);
+            }
+        } else {
+            is_reverse = !is_reverse;
+        }
+    }
+
+    if is_reverse {
+        println!(
+            "{}{}",
+            (n..n * 2).map(|i| s[i]).collect::<String>(),
+            (0..n).map(|i| s[i]).collect::<String>()
+        )
+    } else {
+        println!("{}", s.iter().collect::<String>())
+    }
+}
