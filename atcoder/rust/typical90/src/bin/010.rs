47eba3dee8a2f15fd4682ed56babc08a6f4fe142
k-yomo <kanji.yy@gmail.com>
2021-05-13 23:04:35 +0900

    Solve 010 in typical90

--- atcoder/rust/typical90/src/bin/010.rs
+++ atcoder/rust/typical90/src/bin/010.rs
@@ -5,11 +5,11 @@ use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
 
-use itertools::*;
 use itertools::__std_iter::once;
+use itertools::*;
 use itertools_num::ItertoolsNum;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
 #[macro_export]
@@ -21,8 +21,31 @@ macro_rules! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr
 #[macro_export]
 macro_rules! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
 
-fn main() { 
+fn main() {
     input! {
-        
+        n: usize,
+        cp: [(usize, usize); n],
+        q: usize,
+        lr: [(usize, usize); q],
+    }
+
+    let mut a_cumsum = vec![0; n + 1];
+    let mut b_cumsum = vec![0; n + 1];
+    for (i, (c, p)) in cp.iter().enumerate() {
+        if *c == 1 {
+            a_cumsum[i + 1] = a_cumsum[i] + p;
+            b_cumsum[i + 1] = b_cumsum[i];
+        } else {
+            a_cumsum[i + 1] = a_cumsum[i];
+            b_cumsum[i + 1] = b_cumsum[i] + p;
+        }
+    }
+
+    for (l, r) in lr {
+        println!(
+            "{} {}",
+            a_cumsum[r] - a_cumsum[l - 1],
+            b_cumsum[r] - b_cumsum[l - 1]
+        )
     }
 }
