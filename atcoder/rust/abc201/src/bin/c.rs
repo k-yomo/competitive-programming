1b40a86ac05c2c447cd48a7f7ff0b8316bca3758
k-yomo <kanji.yy@gmail.com>
2021-05-15 22:55:05 +0900

    Solve A,B,C in abc201

---
+++ atcoder/rust/abc201/src/bin/c.rs
@@ -0,0 +1,48 @@
+#![allow(unused_imports)]
+
+use std::cmp::*;
+use std::collections::*;
+use std::io::Write;
+use std::ops::Bound::*;
+
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
+use superslice::*;
+use whiteread::parse_string;
+
+#[macro_export]
+macro_rules! chmin {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_min = min ! ($ ($ cmps ) ,+ ) ; if $ base > cmp_min {$ base = cmp_min ; true } else {false } } } ; }
+#[macro_export]
+macro_rules! chmax {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_max = max ! ($ ($ cmps ) ,+ ) ; if $ base < cmp_max {$ base = cmp_max ; true } else {false } } } ; }
+#[macro_export]
+macro_rules! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: min ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: min ($ a , min ! ($ ($ rest ) ,+ ) ) } } ; }
+#[macro_export]
+macro_rules! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
+
+fn main() {
+    input! {
+        s: Chars,
+    }
+
+    let mut count = 0;
+    for i in (0..=9999).map(|i| format!("{:04}", i)) {
+        let mut exist_nums = vec![false; 10];
+        for c in i.chars() {
+            exist_nums[c.to_digit(10).unwrap() as usize] = true;
+        }
+        let mut is_valid = true;
+        for i in 0..10 {
+            if s[i] == 'o' && !exist_nums[i] || s[i] == 'x' && exist_nums[i] {
+                is_valid = false
+            }
+        }
+        if is_valid {
+            count += 1;
+        }
+    }
+
+    println!("{}", count);
+}
