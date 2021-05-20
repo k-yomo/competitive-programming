641d2e1d2e3b2dbd76341fba53d384ac5f3a124f
k-yomo <kanji.yy@gmail.com>
2021-05-20 22:02:11 +0900

    Solve C in abc108

--- atcoder/rust/abc108/src/bin/c.rs
+++ atcoder/rust/abc108/src/bin/c.rs
@@ -1,13 +1,21 @@
 #![allow(unused_imports)]
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+fn main() {
     input! {
-        
+        n: usize, k: usize,
+    }
+
+    if k % 2 == 0 {
+        // a ≡ b ≡ c ≡ 0 mod K || a ≡ b ≡ c ≡ k/2 mod K
+        println!("{}", (n / k).pow(3) + ((n + k / 2) / k).pow(3))
+    } else {
+        // a ≡ b ≡ c ≡ 0 mod K
+        println!("{}", (n / k).pow(3));
     }
 }
