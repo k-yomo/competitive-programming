bee7047f6ea8cf1fd25fb5f68b51bd038a26c96d
k-yomo <kanji.yy@gmail.com>
2021-06-03 20:30:41 +0900

    Solve 014,016 in typical90

--- atcoder/rust/typical90/src/bin/016.rs
+++ atcoder/rust/typical90/src/bin/016.rs
@@ -5,11 +5,11 @@ use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
 
-use itertools::*;
 use itertools::__std_iter::once;
+use itertools::*;
 use itertools_num::ItertoolsNum;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
 #[macro_export]
@@ -21,8 +21,27 @@ macro_rules! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr
 #[macro_export]
 macro_rules! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
 
-fn main() { 
+fn main() {
     input! {
-        
+        n: usize,
+        a: usize, b: usize, c: usize,
     }
+
+    let mut min_count = 9999;
+    for i in 0..=9999 {
+        if a * i > n {
+            break;
+        }
+        for j in 0..=(9999 - i) {
+            if a * i + b * j > n {
+                break;
+            }
+            let rest = n - (a * i + b * j);
+            if rest % c == 0 {
+                min_count = std::cmp::min(min_count, i + j + rest / c);
+            }
+        }
+    }
+
+    println!("{}", min_count)
 }
