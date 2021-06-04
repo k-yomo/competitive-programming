23e1065cbee6f6dd15ea5c9c0dc8e0d05b2874a1
k-yomo <kanji.yy@gmail.com>
2021-06-05 00:56:16 +0900

    Solve B in arc114

--- atcoder/rust/arc114/src/bin/b.rs
+++ atcoder/rust/arc114/src/bin/b.rs
@@ -17,4 +17,96 @@ fn main() {
         n: usize,
         mut f: [usize; n],
     }
+
+    let mut dsu = Dsu::new(n);
+    for i in 0..n {
+        dsu.merge(i, f[i] - 1);
+    }
+    println!("{}", modpow(2, dsu.groups().len() as u128, 998244353) - 1)
+}
+
+fn modpow(x: u128, e: u128, m: u128) -> u128 {
+    let mut x = x;
+    let mut e = e;
+    let mut res = 1;
+    while e > 0 {
+        if e & 1 != 0 {
+            res *= x;
+            res %= m;
+        }
+        x *= x;
+        x %= m;
+        e >>= 1;
+    }
+    return res;
+}
+
+// https://github.com/rust-lang-ja/ac-library-rs/blob/master/src/dsu.rs
+pub struct Dsu {
+    n: usize,
+    // root node: -1 * component size
+    // otherwise: parent
+    parent_or_size: Vec<i32>,
+}
+
+impl Dsu {
+    // 0 <= size <= 10^8 is constrained.
+    pub fn new(size: usize) -> Self {
+        Self {
+            n: size,
+            parent_or_size: vec![-1; size],
+        }
+    }
+    pub fn merge(&mut self, a: usize, b: usize) -> usize {
+        assert!(a < self.n);
+        assert!(b < self.n);
+        let (mut x, mut y) = (self.leader(a), self.leader(b));
+        if x == y {
+            return x;
+        }
+        if -self.parent_or_size[x] < -self.parent_or_size[y] {
+            std::mem::swap(&mut x, &mut y);
+        }
+        self.parent_or_size[x] += self.parent_or_size[y];
+        self.parent_or_size[y] = x as i32;
+        x
+    }
+
+    pub fn same(&mut self, a: usize, b: usize) -> bool {
+        assert!(a < self.n);
+        assert!(b < self.n);
+        self.leader(a) == self.leader(b)
+    }
+    pub fn leader(&mut self, a: usize) -> usize {
+        assert!(a < self.n);
+        if self.parent_or_size[a] < 0 {
+            return a;
+        }
+        self.parent_or_size[a] = self.leader(self.parent_or_size[a] as usize) as i32;
+        self.parent_or_size[a] as usize
+    }
+    pub fn size(&mut self, a: usize) -> usize {
+        assert!(a < self.n);
+        let x = self.leader(a);
+        -self.parent_or_size[x] as usize
+    }
+    pub fn groups(&mut self) -> Vec<Vec<usize>> {
+        let mut leader_buf = vec![0; self.n];
+        let mut group_size = vec![0; self.n];
+        for i in 0..self.n {
+            leader_buf[i] = self.leader(i);
+            group_size[leader_buf[i]] += 1;
+        }
+        let mut result = vec![Vec::new(); self.n];
+        for i in 0..self.n {
+            result[i].reserve(group_size[i]);
+        }
+        for i in 0..self.n {
+            result[leader_buf[i]].push(i);
+        }
+        result
+            .into_iter()
+            .filter(|x| !x.is_empty())
+            .collect::<Vec<Vec<usize>>>()
+    }
 }
