a3b85d59b6e9ff5cbbff779e0bfd22efca472402
k-yomo <kanji.yy@gmail.com>
2021-06-17 20:28:35 +0900

    Solve 026 in typical90

--- atcoder/rust/typical90/src/bin/026.rs
+++ atcoder/rust/typical90/src/bin/026.rs
@@ -5,24 +5,45 @@ use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
 
-use itertools::*;
 use itertools::__std_iter::once;
+use itertools::*;
 use itertools_num::ItertoolsNum;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
-#[macro_export]
-macro_rules! chmin {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_min = min ! ($ ($ cmps ) ,+ ) ; if $ base > cmp_min {$ base = cmp_min ; true } else {false } } } ; }
-#[macro_export]
-macro_rules! chmax {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_max = max ! ($ ($ cmps ) ,+ ) ; if $ base < cmp_max {$ base = cmp_max ; true } else {false } } } ; }
-#[macro_export]
-macro_rules! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: min ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: min ($ a , min ! ($ ($ rest ) ,+ ) ) } } ; }
-#[macro_export]
-macro_rules! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
-
-fn main() { 
+fn main() {
     input! {
-        
+        n: usize,
+        ab: [(usize, usize); n-1],
+    }
+
+    let mut paths = vec![vec![]; n];
+    for (a, b) in ab {
+        paths[a - 1].push(b - 1);
+        paths[b - 1].push(a - 1);
+    }
+
+    let mut nodes: Vec<usize> = vec![2; n];
+    dfs(&mut nodes, &paths, 0, 0);
+    let i = if nodes.iter().filter(|&&x| x == 0).count() >= n / 2 {
+        0
+    } else {
+        1
+    };
+    nodes
+        .iter()
+        .enumerate()
+        .filter(|(_, x)| **x == i)
+        .take(n / 2)
+        .for_each(|(i, _)| println!("{}", i + 1))
+}
+
+fn dfs(nodes: &mut Vec<usize>, paths: &Vec<Vec<usize>>, pos: usize, cur: usize) {
+    nodes[pos] = cur;
+    for &i in paths[pos].iter() {
+        if nodes[i] == 2 {
+            dfs(nodes, paths, i, 1 - cur)
+        }
     }
 }
