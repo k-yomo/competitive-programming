5c3d304e0731c64bf5595e94e573812658207b59
k-yomo <kanji.yy@gmail.com>
2021-06-18 00:49:49 +0900

    Solve C in abc048

--- atcoder/rust/abc048/src/bin/c.rs
+++ atcoder/rust/abc048/src/bin/c.rs
@@ -1,13 +1,25 @@
 #![allow(unused_imports)]
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+fn main() {
     input! {
-        
+        n: usize, x: usize,
+        mut a: [usize; n],
     }
+
+    let mut count = 0;
+    for i in 1..n {
+        if a[i - 1] + a[i] > x {
+            let diff = a[i - 1] + a[i] - x;
+            count += diff;
+            a[i] -= std::cmp::min(diff, a[i]);
+        }
+    }
+
+    println!("{}", count);
 }
