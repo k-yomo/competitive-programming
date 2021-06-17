b5088a4d9574c4dcdd12c62507215e172411b47f
k-yomo <kanji.yy@gmail.com>
2021-06-18 00:35:56 +0900

    Solve C in abc051

--- atcoder/rust/abc051/src/bin/c.rs
+++ atcoder/rust/abc051/src/bin/c.rs
@@ -1,13 +1,26 @@
 #![allow(unused_imports)]
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
- 
-fn main() { 
+
+fn main() {
     input! {
-        
+        sx: i64, sy: i64, tx: i64, ty: i64,
     }
+
+    (1..=(ty - sy)).for_each(|i| print!("U"));
+    (1..=(tx - sx)).for_each(|i| print!("R"));
+    (1..=(ty - sy)).for_each(|i| print!("D"));
+    (1..=(tx - sx)).for_each(|i| print!("L"));
+
+    print!("L");
+    (1..=(ty - sy) + 1).for_each(|i| print!("U"));
+    (1..=(tx - sx) + 1).for_each(|i| print!("R"));
+    print!("DR");
+    (1..=(ty - sy) + 1).for_each(|i| print!("D"));
+    (1..=(tx - sx) + 1).for_each(|i| print!("L"));
+    print!("U");
 }
