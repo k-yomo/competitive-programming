b90552db4c66bd8fe441ba78873409700562e7c4
k-yomo <kanji.yy@gmail.com>
2021-06-19 18:04:06 +0900

    Solve C in keyence2020

--- atcoder/rust/keyence2020/src/bin/c.rs
+++ atcoder/rust/keyence2020/src/bin/c.rs
@@ -6,12 +6,17 @@ use std::io::Write;
 use std::ops::Bound::*;
 
 use itertools::*;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
-fn main() { 
+fn main() {
     input! {
-        
+        n: usize, k: usize, mut s: usize,
+    }
+
+    (0..k).for_each(|_| print!("{} ", s));
+    for _ in 0..n - k {
+        print!("{} ", if s == 1000000000 { 1 } else { 1000000000 })
     }
 }
