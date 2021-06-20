2374c211548a4828eafb5c7f2ae3373ba97c1763
k-yomo <kanji.yy@gmail.com>
2021-06-20 15:11:02 +0900

    Solve A in cf27-final

--- atcoder/rust/cf17-final/src/bin/a.rs
+++ atcoder/rust/cf17-final/src/bin/a.rs
@@ -5,15 +5,33 @@ use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
 
-use itertools::*;
 use itertools::__std_iter::once;
+use itertools::*;
 use itertools_num::ItertoolsNum;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
 fn main() {
     input! {
-
+        mut s: Chars,
     }
+
+    if s.len() > 9 {
+        return println!("NO");
+    }
+
+    let mut akihabara = String::from("AKIHABARA");
+    let mut i = 0;
+    for c in akihabara.chars() {
+        if i < s.len() && c == s[i] {
+            i += 1;
+        } else {
+            if c == 'A' {
+                continue;
+            }
+            return println!("NO");
+        }
+    }
+    println!("YES")
 }
