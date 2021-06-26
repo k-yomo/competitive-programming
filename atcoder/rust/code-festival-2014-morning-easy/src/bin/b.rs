59401c382114ec359a40ef76dd9527915c597896
k-yomo <kanji.yy@gmail.com>
2021-06-26 11:04:59 +0900

    Solve B in code-festival-2014-morning-easy

---
+++ atcoder/rust/code-festival-2014-morning-easy/src/bin/b.rs
@@ -0,0 +1,20 @@
+#![allow(unused_imports)]
+
+use std::cmp::*;
+use std::collections::*;
+use std::io::Write;
+use std::ops::Bound::*;
+
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
+use superslice::*;
+
+fn main() {
+    input! {
+        mut n: usize,
+    }
+    println!("{}", if n % 40 <= 20 { n % 40 } else { 41 - n % 40 })
+}
