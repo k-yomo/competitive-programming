cf3f47ab5f78f24e7e60339c62067ce891006881
k-yomo <kanji.yy@gmail.com>
2021-08-08 19:04:49 +0900

    Solve B in arc003

--- atcoder/rust/arc003/src/bin/b.rs
+++ atcoder/rust/arc003/src/bin/b.rs
@@ -5,15 +5,26 @@ use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
 
-use itertools::*;
 use itertools::__std_iter::once;
+use itertools::*;
 use itertools_num::ItertoolsNum;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
-fn main() { 
+fn main() {
     input! {
-        
+        n: usize,
+        mut s: [String; n]
+    }
+
+    s = s
+        .iter()
+        .map(|word| word.chars().rev().collect::<String>())
+        .collect::<Vec<String>>();
+    s.sort();
+
+    for rev_word in s {
+        println!("{}", rev_word.chars().rev().collect::<String>())
     }
 }
