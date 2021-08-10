e57ad34ac00831548b12787e5426df24956d0c04
k-yomo <kanji.yy@gmail.com>
2021-08-10 22:16:34 +0900

    Solve A in arc056

---
+++ atcoder/rust/arc056/src/bin/a.rs
@@ -0,0 +1,25 @@
+#![allow(unused_imports)]
+
+use std::cmp::*;
+use std::collections::*;
+use std::io::Write;
+use std::ops::Bound::*;
+
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
+use superslice::*;
+
+fn main() {
+    input! {
+        a: usize, b: usize, k: usize, l: usize,
+    }
+
+    if a * l <= b {
+        println!("{}", k * a)
+    } else {
+        println!("{}", k / l * b + std::cmp::min(b, k % l * a))
+    }
+}
