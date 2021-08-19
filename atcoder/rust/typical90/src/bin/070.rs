bb72d63b2f8921a3568207b52ccd15900393787d
k-yomo <kanji.yy@gmail.com>
2021-08-19 19:31:36 +0900

    Solve 070 in typical90

--- atcoder/rust/typical90/src/bin/070.rs
+++ atcoder/rust/typical90/src/bin/070.rs
@@ -5,15 +5,26 @@ use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
 
-use itertools::*;
 use itertools::__std_iter::once;
+use itertools::*;
 use itertools_num::ItertoolsNum;
-use proconio::*;
 use proconio::marker::*;
+use proconio::*;
 use superslice::*;
 
 fn main() {
     input! {
-
+        n: usize,
+        xy: [(i128, i128); n],
     }
+
+    let fac_x = xy.iter().map(|(x, _)| x).sorted().nth(n / 2).unwrap();
+    let fac_y = xy.iter().map(|(_, y)| y).sorted().nth(n / 2).unwrap();
+
+    let mut total = 0;
+    for (x, y) in xy.iter() {
+        total += (x - fac_x).abs() + (y - fac_y).abs();
+    }
+
+    println!("{}", total);
 }
