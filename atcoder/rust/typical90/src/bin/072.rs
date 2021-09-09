99e0340f15c0a0473a401932f2725895ad2ea575
k-yomo <kanji.yy@gmail.com>
2021-09-09 20:01:54 +0900

    Solve 072 in typical90

--- atcoder/rust/typical90/src/bin/072.rs
+++ atcoder/rust/typical90/src/bin/072.rs
@@ -14,6 +14,42 @@ use superslice::*;
 
 fn main() {
     input! {
-
+        h: usize, w: usize,
+        c: [Chars; h],
     }
+
+    let mut max_length = 0_i64;
+    for (y, x) in iproduct!(0..h, 0..w) {
+        if c[y][x] != '#' {
+            max_length = std::cmp::max(max_length, dfs(&c, &vec![vec![false; w]; h], (y, x), (y, x), 0).unwrap_or(0))
+        }
+    }
+
+    println!("{}", if max_length >= 3 { max_length } else { -1 })
+}
+
+fn dfs(graph: &Vec<Vec<char>>, visited: &Vec<Vec<bool>>, start: (usize, usize), cur: (usize, usize), length: i64) -> Option<i64> {
+    let mut cloned_visited = visited.clone();
+    if cloned_visited[cur.0][cur.1] {
+        if cur.0 == start.0 && cur.1 == start.1 {
+            return Some(length)
+        }
+        return None
+    }
+    cloned_visited[cur.0][cur.1] = true;
+
+    let mut max_length = 0;
+    if cur.0 > 0 && graph[cur.0-1][cur.1] != '#' {
+        max_length = std::cmp::max(max_length, dfs(graph, &cloned_visited, start, (cur.0 -1, cur.1), length + 1).unwrap_or(0))
+    }
+    if cur.1 < graph[0].len() - 1 && graph[cur.0][cur.1 + 1] != '#' {
+        max_length = std::cmp::max(max_length, dfs(graph, &cloned_visited, start, (cur.0, cur.1 + 1), length + 1).unwrap_or(0))
+    }
+    if cur.0 < graph.len() - 1 && graph[cur.0+1][cur.1] != '#' {
+        max_length =  std::cmp::max(max_length, dfs(graph, &cloned_visited, start, (cur.0 + 1, cur.1), length + 1).unwrap_or(0))
+    }
+    if cur.1 > 0 && graph[cur.0][cur.1 - 1] != '#' {
+        max_length = std::cmp::max(max_length, dfs(graph, &cloned_visited, start, (cur.0, cur.1 - 1), length + 1).unwrap_or(0))
+    }
+    return Some(max_length)
 }
