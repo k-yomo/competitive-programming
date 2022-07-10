b2e9059e52be7452b55c4c3f9ac4468d88931582
k-yomo <kanji.yy@gmail.com>
2022-07-10 13:05:26 +0900

    Solve B in abc258

--- atcoder/rust/abc258/src/bin/b.rs
+++ atcoder/rust/abc258/src/bin/b.rs
@@ -2,7 +2,7 @@
 
 use std::cmp::*;
 use std::collections::*;
-use std::io::Write;
+use std::io::{BufRead, Write};
 use std::ops::Bound::*;
 
 use itertools::*;
@@ -11,9 +11,38 @@ use itertools_num::ItertoolsNum;
 use proconio::*;
 use proconio::marker::*;
 use superslice::*;
+use whiteread::parse_string;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [Chars; n],
     }
+
+    let a_nums = a.iter().
+        map(|s| s.iter().map(|c| c.to_string().parse().unwrap()).collect::<Vec<usize>>()).
+        collect::<Vec<Vec<usize>>>();
+
+    let n = n as i64;
+    let mut max_total  = 0usize;
+    for y in 0..n {
+        for x in 0..n {
+            for (y_diff, x_diff) in vec![(1, 0), (0, 1), (1, 1), (0, -1), (-1, 1), (-1, 0), (1, -1), (-1, -1)].iter() {
+                let mut total = 0usize;
+                let mut cur_y = y;
+                let mut cur_x = x;
+                for i in 0..n {
+                    total *= 10;
+                    total += a_nums[cur_y as usize][cur_x as usize];
+                    cur_y = (cur_y + n + y_diff) % n;
+                    cur_x = (cur_x + n + x_diff) % n;
+                }
+                if total > max_total {
+                    max_total = total;
+                }
+            }
+        }
+    }
+
+    println!("{}", max_total);
 }
