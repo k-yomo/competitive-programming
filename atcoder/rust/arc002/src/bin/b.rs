67ddee2e53972e7834d36ab39d859de65615f081
k-yomo <kanji.yy@gmail.com>
2022-07-11 21:13:22 +0900

    Solve B in arc002

--- atcoder/rust/arc002/src/bin/b.rs
+++ atcoder/rust/arc002/src/bin/b.rs
@@ -12,8 +12,36 @@ use proconio::*;
 use proconio::marker::*;
 use superslice::*;
 
-fn main() { 
+fn main() {
     input! {
         ymd: String,
+    }
+
+    let nums = ymd.split("/").
+        collect::<Vec<&str>>().
+        iter().
+        map(|i| i.to_string().parse::<usize>().unwrap()).
+        collect::<Vec<usize>>();
+
+    let mut year = nums[0];
+    let mut month = nums[1];
+    let mut day = nums[2];
+
+    loop {
+        if year % month == 0 && year / month % day == 0 {
+            return println!("{}/{:02}/{:02}", year, month, day);
+        }
+        let is_leap_year = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        if day == 31 || (month == 2 && (day == if is_leap_year { 29 } else { 28 })) || (vec![4, 6, 9, 11].contains(&month) && day == 30) {
+            day = 1;
+            if month == 12 {
+                month = 1;
+                year += 1;
+            } else {
+                month += 1;
+            }
+        } else {
+            day += 1;
+        };
     }
 }
