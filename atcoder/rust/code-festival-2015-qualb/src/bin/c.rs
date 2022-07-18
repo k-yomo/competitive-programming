c04461a9292b55763c5450bab0d4fc96e90b8c00
k-yomo <kanji.yy@gmail.com>
2022-07-18 20:36:43 +0900

    Solve C in code-festival-2015-qualb

--- atcoder/rust/code-festival-2015-qualb/src/bin/c.rs
+++ atcoder/rust/code-festival-2015-qualb/src/bin/c.rs
@@ -14,6 +14,23 @@ use superslice::*;
 
 fn main() {
     input! {
-
+        n: usize, m: usize,
+        mut a: [usize; n],
+        mut b: [usize; m],
     }
+
+    a.sort();
+    b.sort();
+
+    if n < m {
+        return println!("NO");
+    }
+
+    for (i, &people_num) in b.iter().rev().enumerate() {
+        if a[n-1-i] < people_num {
+            return println!("NO");
+        }
+    }
+
+    println!("YES");
 }
